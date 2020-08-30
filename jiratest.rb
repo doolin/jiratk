e38f6d24c3a0d75cd49f722ec49bd54d019153a7
Dave Doolin <david.doolin@gmail.com>
2020-08-30 09:58:46 -0700

    updated jira test to list projects

--- jiratest.rb
+++ jiratest.rb
@@ -9,25 +9,38 @@ require 'aws-sdk-s3'
 require_relative 'lib/account_manager'
 require_relative 'lib/api_helper'
 
-api_url = 'https://doolin.atlassian.net/rest/api/3/search'
-
 api_keys = AccountManager.new.api_keys
 USERNAME = api_keys[:jira_id]
 PASSWORD = api_keys[:jira_key]
-api_helper = ApiHelper.new(api_url)
 
-QUERY = {
-  jql: 'project = "PLANT"',
-  start_at: 1
-}
 
-response = api_helper.get(QUERY)
-response_json = JSON.parse(response)
+def list_issues_for(project)
+  query = {
+    jql: "project = \"#{project}\"",
+    start_at: 1
+  }
 
-ticket_count = response_json['total']
+  api_url = 'https://doolin.atlassian.net/rest/api/3/search'
+  api_helper = ApiHelper.new(api_url)
 
-issues = response_json['issues']
-
-issues.each do |issue|
-  puts issue['key']
+  response = api_helper.get(query)
+  response_json = JSON.parse(response)
+  ticket_count = response_json['total']
+  issues = response_json['issues']
+  issues.each do |issue|
+    puts issue['key']
+  end
 end
+list_issues_for('PLANT')
+
+# Get a list of projects
+def project_list
+  api_url = 'https://doolin.atlassian.net/rest/api/3/project/search'
+  api_helper = ApiHelper.new(api_url)
+  response = api_helper.get({})
+  response_json = JSON.parse(response)
+  response_json['values'].each do |value|
+   puts value['key']
+  end
+end
+project_list
