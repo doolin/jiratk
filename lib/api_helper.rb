f51c4ca0110337dc3b15c87870e9b6153e4e89c8
Dave Doolin <david.doolin@gmail.com>
2020-10-04 07:21:04 -0700

    GEN-23 create an issue on Jira (#3)
    This change is essentially a spike to show how an
    issue may be created using the API.

--- lib/api_helper.rb
+++ lib/api_helper.rb
@@ -5,6 +5,7 @@ require 'rest-client'
 # Add class documentation
 class ApiHelper
   def initialize(api_url)
+    @api_url = api_url
     @resource = RestClient::Resource.new(
       api_url, user: USERNAME, password: PASSWORD
     )
@@ -15,6 +16,22 @@ class ApiHelper
       return resp if (200..499).include? resp.code
 
       resp.return!(req, res, &block)
+    end
+  end
+
+  def post(params)
+    # @resource.post(accept: :json, params: params) do |resp, req, res, &block|
+    # @resource.post(@api_url, payload: params.to_json) do |response, request, res, &block|
+
+    @response = RestClient::Request.new(
+      url: @api_url, user: USERNAME, password: PASSWORD, payload: params.to_json, method: :post,
+      headers: { content_type: 'application/json' }
+    ).execute do |response, request, _result, &block|
+      puts "RESPONSE: #{response.code}"
+      puts "REQUEST: #{request}"
+      return response if (200..499).include? response.code
+
+      response.return!(request, res, &block)
     end
   end
 end
