98de9b2c765e877efc0f2044ca74648f151e253e
dave doolin <david.doolin@gmail.com>
2020-11-21 05:18:53 -0800

    GEN-70 Factor out project listing for Jira account
    Projects in Jira are associated with accounts, so it
    makes sense to have a method for listing project keys
    as a part of the class which manages accounts.
    
    A few other minor cleanups were done, some TODO items
    added, and a few more specs and stubbed specs.

--- lib/jiratk/account_manager.rb
+++ lib/jiratk/account_manager.rb
@@ -10,4 +10,17 @@ class AccountManager
       jira_key: ENV['DOOLIN_JIRA_API']
     }
   end
+
+  def search_url
+    @search_url ||= 'https://doolin.atlassian.net/rest/api/3/project/search'
+  end
+
+  def project_keys
+    api_helper = ApiHelper.new(search_url)
+
+    response = api_helper.get({})
+    response_json = JSON.parse(response)
+
+    response_json['values'].map { |value| value['key'] }
+  end
 end
