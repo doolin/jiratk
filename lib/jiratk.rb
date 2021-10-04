5d7cf2e1e57c0d30a667c0091ecb3333317ea311
Dave Doolin <david.doolin@gmail.com>
2021-10-04 05:05:31 -0700

    GEN-166 fix requires for jiratk (#76)
    For some reason, which I do not recall, the `require`
    syntax broke. This commit fixes that. The changes
    were made weeks ago, were never committed.

--- lib/jiratk.rb
+++ lib/jiratk.rb
@@ -1,5 +1,10 @@
 # frozen-string-literal: true
 
-Dir[File.join(File.dirname(__FILE__), '.', 'jiratk', '**.rb')].sort.each do |f|
-  require f
-end
+# Dir[File.join(File.dirname(__FILE__), '.', 'jiratk', '**.rb')].sort.each do |f|
+#   require f
+# end
+
+require_relative './jiratk/account_manager'
+require_relative './jiratk/api_helper'
+require_relative './jiratk/s3_tools'
+require_relative './jiratk/project'
